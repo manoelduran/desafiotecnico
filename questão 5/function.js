de3917e5774b2473cf96685c665a01d08122b3a9
Manoel Duran <manoel.duran@hotmail.com>
2021-09-30 18:40:09 -0300

    desafio finalizado

---
+++ questão 5/function.js
@@ -0,0 +1,22 @@
+
+const dadosPessoais = [
+    {
+        "Cpf": "00000000000",
+        "Name": "Jose Pedro",
+        "Divida": "0.00",
+        "DataAtualizacao": "26/04/2025",
+        "QtdParcelas": "5",
+        "idSimulacao": "22",
+        "parcelamento": [
+            {
+                "NumeroParcela": "5",
+                "VencimentoParcela": "25/06/2025",
+                "ValorParcela": "20.0"
+            }
+        ],
+        "Spawn": "abbey03",
+        "Time": "12h",
+        "Weakness": "Fantasma",
+        "Size": "Grande"
+    }
+]
